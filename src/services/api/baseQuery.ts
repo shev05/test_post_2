c83d72e9b7055cc151f3c0dfe945a7c9e25c3e7f
Shevchnko Evgeniy <zhenshev05@mail.ru>
2025-05-30 10:13:57 +0300

    fix

--- src/services/api/baseQuery.ts
+++ src/services/api/baseQuery.ts
@@ -5,7 +5,7 @@ import type { Tokens } from '../../shared/constants/types/tokens'
 import { PATHS } from '../../shared/constants/route/routes'
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: '/',
+  baseUrl: process.env.VITE_API_URL,
   credentials: 'include',
 })
 
