e672ab65cd3729f3b31d64465d2b310286839410
Shevchnko Evgeniy <zhenshev05@mail.ru>
2025-05-30 10:28:40 +0300

    fix

--- src/shared/constants/url/api.ts
+++ src/shared/constants/url/api.ts
@@ -2,7 +2,7 @@ export const URL_ADRESS = {
   AUTH_URL: '/auth/login',
   REFRESH_URL: '/auth/refresh',
   REGISTER_URL: '/users/register',
-  USERS_URL: 'https://testtask.public.indev.by/users',
+  USERS_URL: '/users',
   POSTS_URL: '/post',
   POSTS_CREATE_URL: '/post/create',
   COMMENT_URL: '/comments',
