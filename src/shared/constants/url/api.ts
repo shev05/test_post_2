1c862913a78b2a4acb4f5cefcd9a289fbaed9c44
Shevchnko Evgeniy <zhenshev05@mail.ru>
2025-05-30 10:43:14 +0300

    fix

--- src/shared/constants/url/api.ts
+++ src/shared/constants/url/api.ts
@@ -1,10 +1,12 @@
+const BASE_URL = import.meta.env.VITE_API_URL || ''
+
 export const URL_ADRESS = {
-  AUTH_URL: '/auth/login',
-  REFRESH_URL: '/auth/refresh',
-  REGISTER_URL: '/users/register',
-  USERS_URL: '/users',
-  POSTS_URL: '/post',
-  POSTS_CREATE_URL: '/post/create',
-  COMMENT_URL: '/comments',
-  COMMENT_CREATE_URL: '/comments/create',
+  AUTH_URL: `${BASE_URL}/auth/login`,
+  REFRESH_URL: `${BASE_URL}/auth/refresh`,
+  USERS_URL: `${BASE_URL}/users`,
+  POSTS_URL: `${BASE_URL}/post`,
+  REGISTER_URL: `${BASE_URL}/users/register`,
+  POSTS_CREATE_URL: `${BASE_URL}/post/create`,
+  COMMENT_URL: `${BASE_URL}/comments`,
+  COMMENT_CREATE_URL: `${BASE_URL}/comments/create`,
 }
